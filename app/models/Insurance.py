1d41f2c69d8e2fee83a5e7d0b23a07736397b245
LAPTOP-MPB2AJ6E\Lenovo <c14210013@john.petra.ac.id>
2024-05-30 11:46:59 +0700

    add insurance models, controller, php

--- app/models/Insurance.py
+++ app/models/Insurance.py
@@ -20,21 +20,21 @@ class Insurance:
         return [
             {
                 "id_user":"001",
-                "nama_user": "Ella Arminta",
+                "nama_user": "Ella ",
                 "nomor_telepon": "1234567890",
-                "alamat": "Bumi"
+                "alamat": "Bumi",
             },
             {
                 "id_user":"002",
                 "nama_user": "Michael Adi",
                 "nomor_telepon": "1234567890",
-                "alamat": "Bumi"
+                "alamat": "Bumi",
             },
             {
                 "id_user":"003",
                 "nama_user": "Yuandi Vick",
                 "nomor_telepon": "1234567890",
-                "alamat": "Bumi"
+                "alamat": "Bumi",
             }
         ]
     
@@ -119,10 +119,98 @@ class Insurance:
         ]
     
     def get_all_klaim_asuransi(self):
-        return
+        return [
+            {
+                "id_klaim_asuransi":"001",
+                "id_user":"001",
+                "id_tipe_asuransi":"001",
+                "id_pembelian":"001",
+                "id_pembayaran":"001",
+                "link_bukti":"testlink1",
+                "date_time":"10-10-2010 10:10:10",
+                "status_klaim":"1",
+            },
+            {
+                "id_klaim_asuransi":"002",
+                "id_user":"002",
+                "id_tipe_asuransi":"002",
+                "id_pembelian":"002",
+                "id_pembayaran":"002",
+                "link_bukti":"testlink2",
+                "date_time":"11-11-2011 10:10:10",
+                "status_klaim":"1",
+            },
+            {
+                "id_klaim_asuransi":"003",
+                "id_user":"003",
+                "id_tipe_asuransi":"003",
+                "id_pembelian":"003",
+                "id_pembayaran":"003",
+                "link_bukti":"testlink3",
+                "date_time":"12-12-2012 10:10:10",
+                "status_klaim":"1",
+            }
+        ]
     def get_all_pembayaran_asuransi(self):
-        return
+        return [
+            {
+                "id_pembayaran":"001",
+                "id_user":"001",
+                "id_pembelian":"001",
+                "timestamp": "05-05-2010 10:10:10",
+                "total_bayar":"100000",
+                "pajak":"1000",
+                "jenis_pembayaran":"1",
+                "nomor_kartu":"55667788",
+                "nomor_telepon":"1234567890",
+            },
+            {
+                "id_pembayaran":"002",
+                "id_user":"002",
+                "id_pembelian":"002",
+                "timestamp": "06-05-2010 10:10:10",
+                "total_bayar":"50000",
+                "pajak":"1000",
+                "jenis_pembayaran":"1",
+                "nomor_kartu":"88990011",
+                "nomor_telepon":"1234567890",
+            },
+            {
+                "id_pembayaran":"003",
+                "id_user":"003",
+                "id_pembelian":"003",
+                "timestamp": "07-05-2010 10:10:10",
+                "total_bayar":"20000",
+                "pajak":"1000",
+                "jenis_pembayaran":"1",
+                "nomor_kartu":"11223344",
+                "nomor_telepon":"1234567890",
+            }
+            
+        ]
     def get_all_coverage_asuransi(self):
-        return
+        return [
+            {
+                "id_coverage_asuransi":"001",
+                "id_tipe_asuransi":"001",
+                "coverage":"abcdefg",
+                "detail":"ini detail",
+                "status":"1"
+            },
+            {
+                "id_coverage_asuransi":"002",
+                "id_tipe_asuransi":"002",
+                "coverage":"hijklmn",
+                "detail":"ini detail",
+                "status":"1"
+            },
+            {
+                "id_coverage_asuransi":"002",
+                "id_tipe_asuransi":"002",
+                "coverage":"opqrstu",
+                "detail":"ini detail",
+                "status":"1"
+            },
+        ]
     
 
