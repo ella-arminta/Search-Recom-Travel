ed5d6e55ad978a458644f33f3c6970b83e612054
Ella Arminta <85121822+ella-arminta@users.noreply.github.com>
2024-06-04 18:44:14 +0700

    sdf

--- gateway.py
+++ gateway.py
@@ -6,3 +6,11 @@ from nameko.web.handlers import http
 
 class GatewayService:
     name = 'gateway'
+    searhrecom_rpc = RpcProxy('searchrecom')
+
+# Hotel
+# Transportasi
+# Travel Agent
+# Atraksi
+# Airlines
+# Insurance
