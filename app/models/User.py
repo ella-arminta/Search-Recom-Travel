f458ccca3704a94cacddb41c88e7f4ae12dfbf80
Ella Arminta <85121822+ella-arminta@users.noreply.github.com>
2024-06-04 18:41:09 +0700

    feat: Ganti jadi nameko

--- app/models/User.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import model
-from app import db
-
-# Defining the Book model
-class User(db.Model):
-    id  = db.Column(db.Integer(), primary_key=True, nullable=False)
-    name = db.Column(db.String(20), nullable=False)
-    age = db.Column(db.Integer, nullable=False)
-    address = db.Column(db.String(100), nullable=False)
