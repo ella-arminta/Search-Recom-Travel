391429a0564a058cd833fa3654594c00ace4d81c
LAPTOP-MPB2AJ6E\Lenovo <c14210013@john.petra.ac.id>
2024-06-08 22:22:07 +0700

    nambah airline service

--- airlines.py
+++ airlines.py
@@ -1,5 +1,5 @@
 from nameko.rpc import rpc
-
+import requests
 import dependencies
 
 class AirlinesService:
@@ -8,7 +8,57 @@ class AirlinesService:
 
     database = dependencies.Database()
 
+    # @rpc
+    # def get_all_airline(self,nama_maskapai):
+    #     airlines_service = self.database.get_service_by_type(2)
+    #     return airlines_service
     @rpc
     def get_all_airlines(self):
-        airlines_service = self.database.get_service_by_type(2)
-        return airlines_service
+        flights_services = self.database.get_service_by_type(2)
+        data=[]
+        
+        for flights_service in flights_services:
+            endpoint_url = flights_service['url']
+            
+            #get all flights
+            try:
+                 # /airlines
+                response = requests.get(endpoint_url)
+                response.raise_for_status()
+                data = response.json()
+                # data yang diterima bentuknya : DUMMY
+                # PERTANYAAN apakah hotel masih menyimpan hotel detail
+                # yang ngecek apakah roomnya avail atau gk itu dari function hotel, atau function search&Recom
+                data = [
+                    {   
+                        'flight_id':'1',
+                        'flight_code_id':'ID123',
+                        'class_id':'1',
+                        'airport_id':'1',
+                        'capacity':100,
+                        'date':'01/01/2001',
+                        'weight':50,
+                        'delay':100,
+                        'status':True
+                    },
+                    {   
+                        'flight_id':'2',
+                        'flight_code_id':'ID123',
+                        'class_id':'2',
+                        'airport_id':'2',
+                        'capacity':100,
+                        'date':'02/02/2002',
+                        'weight':50,
+                        'delay':100,
+                        'status':True
+                    },
+                ]
+                
+            except requests.exceptions.RequestException as e:
+                # Handle any exceptions that occur during the request
+                # TODO add error to database
+                continue
+        return{
+            'code':200,
+            'data':data
+        }
