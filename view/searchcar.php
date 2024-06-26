5abbbb0b107c3135a8d416fe30fff58524cc0464
Ella Arminta <85121822+ella-arminta@users.noreply.github.com>
2024-06-26 09:54:14 +0700

    feat: done searchcar view

--- view/searchcar.php
+++ view/searchcar.php
@@ -35,11 +35,11 @@
                 <!-- radio with or withou driver -->
                 <div class="flex items-center gap-4">
                     <div class="flex items-center gap-2">
-                        <input type="radio" name="driver" checked id="withdriver" value="withdriver" class="w-5 h-5" />
+                        <input type="radio" name="driver" checked id="withdriver" value="1" class="w-5 h-5" />
                         <label for="withdriver">With Driver</label>
                     </div>
                     <div class="flex items-center gap-2">
-                        <input type="radio" name="driver" id="withoutdriver" value="withoutdriver" class="w-5 h-5" />
+                        <input type="radio" name="driver" id="withoutdriver" value="0" class="w-5 h-5" />
                         <label for="withoutdriver">Without Driver</label>
                     </div>
                 </div>
@@ -67,7 +67,7 @@
                 <div class="w-1/3 text-gray-500">
                     <label for="startdate" class="pb-3">Rental Start Date</label>
                     <div class="relative w-full mt-2">
-                        <input type="date" name="startdate" id="startdate" class="date-input w-full p-2 pl-10 rounded border border-gray-300 focus:bg-white focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:border-transparent" placeholder="search..." />
+                        <input type="date" name="startdate" id="checkin" class="date-input w-full p-2 pl-10 rounded border border-gray-300 focus:bg-white focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:border-transparent" placeholder="search..." />
                         <svg class="absolute top-2 left-2" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" data-id="IcSystemCalendar">
                             <path d="M7 2V5M17 2V5M3 8H21M11.5 11.5H12.5V12.5H11.5V11.5ZM11.5 16.5H12.5V17.5H11.5V16.5ZM16.5 11.5H17.5V12.5H16.5V11.5ZM6.5 16.5H7.5V17.5H6.5V16.5ZM5 21H19C20.1046 21 21 20.1046 21 19V6C21 4.89543 20.1046 4 19 4H5C3.89543 4 3 4.89543 3 6V19C3 20.1046 3.89543 21 5 21Z" stroke="#687176" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
                             <path fill-rule="evenodd" clip-rule="evenodd" d="M7.5 11.5V12.5H6.5V11.5H7.5Z" stroke="#0194F3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
@@ -78,17 +78,17 @@
                 <div class="w-1/3">
                     <label for="enddate" class="pb-3 text-gray-500">Rental End Date</label>
                     <div class="relative w-full mt-2">
-                        <input type="date" name="enddate" id="enddate" class="w-full p-2 pl-10 rounded border border-gray-300 focus:bg-white focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:border-transparent" placeholder="search..." />
+                        <input type="date" name="enddate" id="checkout" class="w-full p-2 pl-10 rounded border border-gray-300 focus:bg-white focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:border-transparent" placeholder="search..." />
                         <img class="absolute top-2 left-2" src="https://d1785e74lyxkqq.cloudfront.net/_next/static/v2/e/e0a90245b7343af73aa2eb421c0d8d55.svg" width="24" height="24">
                     </div>
                 </div>
                 <div class="w-1/3 flex items-end">
                     <!-- Button search -->
-                    <a href="cars.php" class="w-full">
-                        <button class="w-full text-white p-2 rounded bg-orange-600 hover:bg-orange-700	font-bold flex items-center justify-center gap-1"><img src="https://d1785e74lyxkqq.cloudfront.net/_next/static/v2/6/68a17a4492b3b7647bb89a5a03b15de0.svg">
+                    <!-- <a href="cars.php" class="w-full"> -->
+                        <button id="search" class="w-full text-white p-2 rounded bg-orange-600 hover:bg-orange-700	font-bold flex items-center justify-center gap-1"><img src="https://d1785e74lyxkqq.cloudfront.net/_next/static/v2/6/68a17a4492b3b7647bb89a5a03b15de0.svg">
                             <div>Search Car Rental</div>
                         </button>
-                    </a>
+                    <!-- </a> -->
                 </div>
             </div>
         </div>
@@ -96,19 +96,28 @@
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script>
         $(document).ready(function() {
-            $('#city_id').on('input', function() {
+            var city_ids = {};
+            $('#city_id').on('input', function(){
                 var search = $(this).val();
-                if (search != '') {
-                    // $.ajax({
-                    //     url: 'search.php',
-                    //     type: 'POST',
-                    //     data: {search:search},
-                    //     success: function(response){
-                    //         $('#cityList').html(response);
-                    $('#cityList').removeClass('hidden')
-                    console.log('searching...');
-                    //     }
-                    // });
+                if(search != ''){
+                    $.ajax({
+                        url: 'http://107.20.145.163:8003/lokasi/'+ search,
+                        type: 'GET',
+                        success: function(response){
+                            hasil = ''
+                            for (var i = 0; i < response.data.length; i++) {
+                                city_ids[response.data[i].nama_kota] = response.data[i].id;
+                                console.log(response.data[i].nama_kota)
+                                hasil+= `<li class="pl-8 pr-2 py-1 border-gray-100 relative cursor-pointer hover:bg-yellow-50 hover:text-gray-900">
+                                    `+response.data[i].nama_kota+`
+                                </li>`
+                                
+                            }
+                            $('#cityList').html(hasil);
+                            $('#cityList').removeClass('hidden')
+                            console.log('searching...');
+                        }
+                    });
                 }
             });
             $(document).on('click', 'li', function() {
@@ -152,6 +161,17 @@
                 if (checkout != 'NaN-NaN-NaN') {
                     $('#checkout').val(checkout);
                 }
+            });
+
+            // button search on click
+            $('#search').on('click', function() {
+                var city = $('#city_id').val();
+                city = city_ids[city];
+                var checkin = $('#checkin').val();
+                var checkout = $('#checkout').val();
+                var driver = $('input[name=driver]:checked').val();
+                window.location.href = `cars.php?city=${city}&checkin=${checkin}&checkout=${checkout}&withDriver=${driver}`;
+
             });
 
         })
