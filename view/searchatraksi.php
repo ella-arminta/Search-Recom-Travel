b66ad6cfa469c425500768bd9240e66c75e61dfc
MichaelAdiPratama <c14210016@john.petra.ac.id>
2024-06-26 13:53:21 +0700

    update searchatraksi dan atraksi php

--- view/searchatraksi.php
+++ view/searchatraksi.php
@@ -60,13 +60,10 @@
                     <div class="relative w-full mt-2">
                         <!-- <input type="text" name="city" id="city" class="date-input w-full p-2 pl-10 rounded border border-gray-300 focus:bg-white focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:border-transparent" placeholder="Kota..." /> -->
                         <select name="city" id="city" class="w-full p-2 pl-10 rounded border border-gray-300 focus:bg-white focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:border-transparent">
-                            <?php
-                        
-                                    echo "<option value='$Malang'>Batu, Malang</option>";
-                                    echo "<option value='$Pasuruan'>Prigen, Pasuruan</option>";
-                                    echo "<option value='$Bali'>Jakarta</option>";
-                                    echo "<option value='$Jakarta'>Bali</option>";
-                            ?>
+                            <option value="Batu">Batu</option>
+                            <option value="Prigen">Prigen</option>
+                            <option value="Jakarta">Jakarta</option>
+                            <option value="Bali">Bali</option>
                         </select>
                         <img class="w-6 h-6 absolute left-2 top-2" src="https://d1785e74lyxkqq.cloudfront.net/_next/static/v2/7/7f57d24fd3db681418a3694bd71cb93b.svg" width="24" height="24">
                     </div>
@@ -75,7 +72,7 @@
                 <div class="w-1/3">
                     <label for="tanggal" class="pb-3 text-gray-500">Date</label>
                     <div class="relative w-full mt-2">
-                        <input type="date" name="tanggal" id="tanggal" class="w-full p-2 pl-10 rounded border border-gray-300 focus:bg-white focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:border-transparent" placeholder="search..." />
+                        <input type="date" name="tanggal" id="tanggal" class="date-input w-full p-2 pl-10 rounded border border-gray-300 focus:bg-white focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:border-transparent" placeholder="search..." />
                         <svg class="absolute top-2 left-2" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" data-id="IcSystemCalendar"><path d="M7 2V5M17 2V5M3 8H21M11.5 11.5H12.5V12.5H11.5V11.5ZM11.5 16.5H12.5V17.5H11.5V16.5ZM16.5 11.5H17.5V12.5H16.5V11.5ZM6.5 16.5H7.5V17.5H6.5V16.5ZM5 21H19C20.1046 21 21 20.1046 21 19V6C21 4.89543 20.1046 4 19 4H5C3.89543 4 3 4.89543 3 6V19C3 20.1046 3.89543 21 5 21Z" stroke="#687176" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M7.5 11.5V12.5H6.5V11.5H7.5Z" stroke="#0194F3" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
                     </div>
                 </div>
